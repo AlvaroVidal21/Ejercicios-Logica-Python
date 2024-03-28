c747ff4ba277ad8526b58e37e21556adf938df2d
AlvaVidal <alvaro.vidal1403@gmail.com>
2024-03-28 08:30:54 -0500

    os.system acorde al sistema operativo

--- analisis_comparativo_jugadores/main.py
+++ analisis_comparativo_jugadores/main.py
@@ -1,5 +1,7 @@
 import time
 import os
+import platform
+
 from menu.interfaz import interfaz
 from estadisticos.show_all_players import show_all_players
 
@@ -7,17 +9,23 @@ from estadisticos.show_all_players import show_all_players
 # Path::data
 TRACK = "data/jugadores.json"
 
+# Operative System
+sistema_operativo = platform.system()
+if sistema_operativo == "Linux":
+    delete = "clear"
+elif sistema_operativo == "Windows":
+    delete = "cls"
 
 def doing():
     option = interfaz()
     if option == 1:
         time.sleep(2)
-        os.system("clear")
+        os.system(delete)
         show_all_players(TRACK)
     else:
         print("Option not available")
         time.sleep(2)
-        os.system("clear")
+        os.system(delete)
         doing()
 
 
