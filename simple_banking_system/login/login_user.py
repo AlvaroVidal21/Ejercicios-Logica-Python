43481bbbdb5a4143fc6ab0bc12b16a49b6373ffe
Alvaro Vidal <alvaro.vidal1403@gmail.com>
2024-03-30 10:39:28 -0500

    Implementacion de func login user

---
+++ simple_banking_system/login/login_user.py
@@ -0,0 +1,18 @@
+
+
+def login_user_fn(read_json_fn, track):
+    attemps = 0
+    if attemps == 3:
+        print("Demasiados intentos fallidos")
+        return None
+    data = read_json_fn(track)
+    user = input("Ingrese su usuario: ")
+    password = input("Ingrese su contraseña: ")
+    for user_data in data:
+        if user_data["user"] == user and user_data["password"] == password:
+            print("Bienvenido")
+            return True
+        else:
+            print("Usuario o contraseña incorrectos")
+            attemps += 1
+            login_user_fn(read_json_fn, track)
--- simple_banking_system/main.py
+++ simple_banking_system/main.py
@@ -1,17 +1,13 @@
 from iu.welcome import print_welcome
+# LOGIN
 from login.register import register_new_user
 from login.add_user_to_data import add_new_user
+from login.login_user import login_user_fn
+# JSON
 from controller_json.read_json import read_json_fn
 from controller_json.write_json import write_json_fn
+
 import json
-
-
-
-
-
-
-
-
 
 
 
@@ -21,15 +17,14 @@ def doing():
     track = "users/all_users.json"
     opcion_de_usuario = print_welcome()
 
-    if opcion_de_usuario == 2:
+    if opcion_de_usuario == 1:
+        login_user_fn(read_json_fn, track)
+    elif opcion_de_usuario == 2:
         nombre, apellido, dni, user, password = register_new_user()
         add_new_user(nombre, apellido, dni, user, password, read_json_fn, write_json_fn, track)
 
 
-
-    add_new_user(nombre, apellido, dni, user, password, read_json_fn, write_json_fn, track)
-
-    # read_json_fn(track)
+    
 
 
 
