6697bdda3328102062a801bcc888e5ad6d85fdc6
Alvaro Vidal <alvaro.vidal1403@gmail.com>
2024-04-25 11:02:40 -0500

    feature clean, interface input

---
+++ finance_management/interfaces/interface_input.py
@@ -0,0 +1,9 @@
+
+
+def interface_input_fn():
+    title = "Finance Management System"
+    print("=" * len(title))
+    print(title)
+    print("=" * len(title))
+    print("\nIngrese sus ingresos totales: ")
+    income = float(input(">>>_ ").strip())
---
+++ finance_management/main.py
@@ -0,0 +1,18 @@
+# INTERFACES
+from interfaces.interface_input import *
+
+# FEATURES
+from features.clean_interface import *
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    clean_interface_fn(1)
+    interface_input_fn()
